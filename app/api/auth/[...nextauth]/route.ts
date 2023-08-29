0a75228dbe01dd9a8c9e735cc3bcb28675e76771
kjetilge <geirbo@icloud.com>
2023-08-29 16:47:52 +0200

    fixed attributes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,43 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
-import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+const FEIDE_API_BASE_URL = 'https://auth.dataporten.no';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+    {
+      id: "feide",
+      name: "Feide",
+      type: "oauth",
+      wellKnown: `${FEIDE_API_BASE_URL}/.well-known/openid-configuration`,
+      authorization: { params: { scope: "openid email profile" } },
+
+      token: `${FEIDE_API_BASE_URL}/oauth/token`,
+      userinfo: `${FEIDE_API_BASE_URL}/v2/user/me`,
+      clientId: process.env.FEIDE_ID,
+      clientSecret: process.env.FEIDE_SECRET,
+      profile(profile) {
+        console.log('********************************** profile', profile.name)
+        return {
+          id: profile.sub,
+          email: profile.sub,
+          name: profile.name
+        }
+      },
+    },
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    })
   ],
 };
 
