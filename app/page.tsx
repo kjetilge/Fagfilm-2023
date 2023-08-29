7f651431fd35cfeb876d7d3771a077a56d4dfc8b
kjetilge <geirbo@icloud.com>
2023-08-29 19:04:13 +0200

    email host

--- app/page.tsx
+++ app/page.tsx
@@ -24,20 +24,11 @@ export default async function Home() {
     .then((res) => res.json())
     .catch((e) => console.log(e));
 
+    const emailhost = process.env.EMAIL_SERVER_HOST
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <a
-          href="https://twitter.com/steventey/status/1613928948915920896"
-          target="_blank"
-          rel="noreferrer"
-          className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-          <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Precedent
-          </p>
-        </a>
+        <p>{emailhost}</p>
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
