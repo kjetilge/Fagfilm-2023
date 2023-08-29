ef60d36005ad2e35f56519043c14f494d64ed34c
kjetilge <geirbo@icloud.com>
2023-08-29 16:47:23 +0200

    fixed multiple attr

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -18,9 +18,11 @@ const SignInModal = ({
   setShowSignInModal: Dispatch<SetStateAction<boolean>>;
 }) => {
   const [signInClicked, setSignInClicked] = useState(false);
+  const [email, setEmail] = useState("");
 
   return (
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
+      <p>test</p>
       <div className="w-full overflow-hidden shadow-xl md:max-w-md md:rounded-2xl md:border md:border-gray-200">
         <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-6 pt-8 text-center md:px-16">
           <a href="https://precedent.dev">
@@ -39,7 +41,45 @@ const SignInModal = ({
           </p>
         </div>
 
+
         <div className="flex flex-col space-y-4 bg-gray-50 px-4 py-8 md:px-16">
+
+        <button
+            disabled={signInClicked}
+            className={`${
+              signInClicked
+                ? "cursor-not-allowed border-gray-200 bg-gray-100"
+                : "border border-gray-200 bg-white text-black hover:bg-gray-50"
+            } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
+            onClick={() => {
+              setSignInClicked(true);
+              signIn("feide");
+            }}
+          >
+            {signInClicked ? (
+              <LoadingDots color="#808080" />
+            ) : (
+              <>
+                <Image src="/feide.png" className="h-5 w-5" alt="feide logo" width={80} height={80} />
+                <p>Logg inn med Feide</p>
+              </>
+            )}
+          </button>
+          <div className="flex h-10 w-full items-center justify-center"><h3>Eller med epost</h3></div>
+
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="email" className="text-sm text-gray-500">
+              Epost
+            </label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              id="email"
+              className="border border-gray-200 rounded-md h-10 px-3 text-sm focus:outline-none focus:ring-2 focus:ring-gray-200"
+              placeholder="Epost"
+            />
+          </div>
           <button
             disabled={signInClicked}
             className={`${
@@ -49,7 +89,7 @@ const SignInModal = ({
             } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
             onClick={() => {
               setSignInClicked(true);
-              signIn("google");
+              signIn("email", { email });
             }}
           >
             {signInClicked ? (
@@ -57,7 +97,7 @@ const SignInModal = ({
             ) : (
               <>
                 <Google className="h-5 w-5" />
-                <p>Sign In with Google</p>
+                <p>Logg inn med epost</p>
               </>
             )}
           </button>
