7e2df3885d5289f9051861b787e307b0ae311362
kjetilge <geirbo@icloud.com>
2023-10-04 21:28:14 +0200

    fix type errors

--- app/filmer/thumbs.ts
+++ app/filmer/thumbs.ts
@@ -1,10 +1,10 @@
 const IMAGESERVER = 'https://d1j2lkoxx8uycr.cloudfront.net/'
 
-export const ToSMPTEu = function (secs) {
+export const ToSMPTEu = function (secs: string) {
   return ToSMPTE(secs).split(':').join('_')
 }
 // returnerer vanlig: 00:12:12:05
-const ToSMPTE = function (secs) {
+const ToSMPTE = function (secs: string) {
   const secNum = parseInt(secs, 10)
   const hours = Math.floor(secNum / 3600) % 24
   const minutes = Math.floor(secNum / 60) % 60
@@ -12,21 +12,21 @@ const ToSMPTE = function (secs) {
   const SMPTEu3 = [hours, minutes, seconds]
     .map(v => v < 10 ? '0' + v : v)
     .join(':')
-  let tenths = (parseFloat(secs) % 1).toFixed(2)
+  let tenths = parseFloat((parseFloat(secs) % 1).toFixed(2))
   tenths = tenths >= 0.96 ? 0.96 : tenths
-  let frames = parseInt((tenths * 25).toFixed(0))
-  frames = frames < 10 ? '00' : frames
+  let framesNum = parseInt((tenths * 25).toFixed(0))
+  const frames = framesNum < 10 ? '00' : framesNum.toString()
   const SMPTE = SMPTEu3 + ':' + String(frames)
   return SMPTE
 }
 
-export const secondsToMMSS = (secs) => {
+export const secondsToMMSS = (secs: string) => {
   const mmss = ToSMPTE(secs).split(':')
   return `${mmss[1]}:${mmss[2]}`
 }
 
-export const thumb = (fileName, time, width) => {
-  const SMTPu = ToSMPTEu(time)
+export const thumb = (fileName:String, time: number, width: string) => {
+  const SMTPu = ToSMPTEu(time.toString())
   const imageUrl = IMAGESERVER + 'SMPTE_' + SMTPu + ',w_' + width + '/' + fileName + '.jpg'
   return imageUrl
 }
