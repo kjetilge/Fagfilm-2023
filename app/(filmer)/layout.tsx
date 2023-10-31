b35fb1b71a26a5731acbd77bd5a08b4ade1f9942
kjetilge <geirbo@icloud.com>
2023-10-31 01:48:16 +0100

    fix metedata

--- app/(filmer)/layout.tsx
+++ app/(filmer)/layout.tsx
@@ -1,26 +1,26 @@
 import "@/app/globals.css";
-import { Analytics } from "@vercel/analytics/react";
+// import { Analytics } from "@vercel/analytics/react";
 import cx from "classnames";
 import { sfPro, inter } from "@/app/fonts";
-import Nav from "@/components/layout/nav";
-import Footer from "@/components/layout/footer";
-import { Suspense } from "react";
+// import Nav from "@/components/layout/nav";
+// import Footer from "@/components/layout/footer";
+// import { Suspense } from "react";
 
 
-export const metadata = {
-  title: "Fagfilm",
-  description:
-    "Fagfilm git deg filmer om videregående utdanning og yrkesvalg.",
-  twitter: {
-    card: "summary_large_image",
-    title: "Fagfilm - Filmer om videregående utdanning og yrkesvalg",
-    description:
-      "Fagfilm - Filmer om videregående utdanning og yrkesvalg.",
-    creator: "@fagfilm",
-  },
-  metadataBase: new URL("https://fagfilm.dev"),
-  themeColor: "#FFF",
-};
+// export const metadata = {
+//   title: "Fagfilm",
+//   description:
+//     "Fagfilm gir deg filmer om videregående utdanning og yrkesvalg.",
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Fagfilm - Filmer om videregående utdanning og yrkesvalg",
+//     description:
+//       "Fagfilm - Filmer om videregående utdanning og yrkesvalg.",
+//     creator: "@fagfilm",
+//   },
+//   metadataBase: new URL("https://fagfilm.dev"),
+//   themeColor: "#FFF",
+// };
 
 export default async function RootLayout({
   children,
