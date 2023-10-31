94c1ade6c6c780510fede004f83241a46b7ed10d
kjetilge <geirbo@icloud.com>
2023-10-31 13:09:31 +0100

    test graphql

--- app/(hjemmesider)/page.tsx
+++ app/(hjemmesider)/page.tsx
@@ -6,72 +6,43 @@ import { Github, Twitter } from "@/components/shared/icons";
 // import ComponentGrid from "@/components/hjemmesider/home/component-grid";
 import Image from "next/image";
 import { nFormatter } from "@/lib/utils";
+import getVideoBySlug from "@/lib/graphql-requests/get-video-by-slug";
+
+const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT as string
+const GRAPHQL_API_KEY = process.env.GRAPHQL_API_KEY as string
+const AWS_REGION = process.env.AWS_REGION as string
 
 export default async function Home() {
-
+  const video = await getVideoBySlug('naturbruk')
   // const session = await getServerSession(authOptions)
   
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-
-
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
           <Balancer>Fagfilm</Balancer>
         </h1>
+        <p>
+        GRAPHQL_ENDPOINT: {GRAPHQL_ENDPOINT}, GRAPHQL_API_KEY: {GRAPHQL_API_KEY}, AWS_REGION: {AWS_REGION}
+        </p>
 
-
+        <p>
+          VIDEO funnet: {video.title}
+        </p>
+        
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
+            
           <Balancer>
             Her er utvikles fagfilm sin neste generasjon nettsted
           </Balancer>
         </p>
 
-
-        {/* <div
-          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
-          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
-        >
-          <a
-            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href={DEPLOY_URL}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg
-              className="h-4 w-4 group-hover:text-black"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 4L20 20H4L12 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <p>Så gøy!</p>
-          </a>
-          <a
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/kjetilge/Fagfilm-2023"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Github />
-            <p>
-              <span className="hidden sm:inline-block">Lagres på</span> GitHub{" "}
-            </p>
-          </a>
-        </div> */}
       </div>
 
     </>
