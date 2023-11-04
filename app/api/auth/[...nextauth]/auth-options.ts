847183c45fc0d40e89ef305d99bd08de52cb17ff
kjetilge <geirbo@icloud.com>
2023-11-04 20:03:46 +0100

    fix callback type err

--- app/api/auth/[...nextauth]/auth-options.ts
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -10,6 +10,10 @@ import { Profiler } from "react";
 
 const FEIDE_API_BASE_URL = 'https://auth.dataporten.no';
 
+type FeideProfile = {
+  sub: string
+  name: string
+}
 
 export const authOptions: NextAuthOptions = {
   // adapter: PrismaAdapter(prisma),
@@ -25,47 +29,13 @@ export const authOptions: NextAuthOptions = {
       userinfo: `${FEIDE_API_BASE_URL}/v2/user/me`,
       clientId: process.env.FEIDE_ID,
       clientSecret: process.env.FEIDE_SECRET,
-      profile(profile) {
+      profile(profile: FeideProfile) {
         return {
           id: profile.sub,
           email: profile.sub,
           name: profile.name,
         }
       },
-    },
-
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: Number(process.env.EMAIL_SERVER_PORT),
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD,
-    //     },
-    //   },
-    //   from: process.env.EMAIL_FROM,
-    // })
+    }
   ],
-  callbacks: {
-    callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        console.log("signIn", { user, account, profile, email, credentials })
-        return true
-      }
-    }
-  },
-  events: {
-    updateUser: async (message) => {  
-      console.log("updateUser", message)
-    },
-    // signOut: async (message) => {
-    //   console.log("signOut", message)
-    // },
-    // createUser: async (message) => {
-    //   console.log("createUser", message)
-    // },
-    signIn: async (message) => {
-      console.log("signIn EVENT: ", message)
-    }
-  },
-};
+}
