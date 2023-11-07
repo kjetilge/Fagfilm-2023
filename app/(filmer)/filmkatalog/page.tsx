4bd4db269adb4d1b40a907248b4c8ea943ef4fb7
kjetilge <geirbo@icloud.com>
2023-11-07 04:53:39 +0100

    loading files

--- app/(filmer)/filmkatalog/page.tsx
+++ app/(filmer)/filmkatalog/page.tsx
@@ -1,8 +1,13 @@
 import Categories from '@/components/filmer/filmkatalog/categories/categories'
+import { Suspense } from 'react'
+// export const runtime = 'edge'
 
 const Filmkatalog = () => {
   return (
+  <Suspense fallback={<p>Laster filmer...</p>}>
     <Categories />
+  </Suspense>
+    
   )
 }
 
