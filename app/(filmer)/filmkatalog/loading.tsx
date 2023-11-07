55a4bb01f2243f1236f9a514f6672943a9cc64da
kjetilge <geirbo@icloud.com>
2023-11-07 05:33:43 +0100

    mer loading

--- app/(filmer)/filmkatalog/loading.tsx
+++ app/(filmer)/filmkatalog/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
-      <h1 className="mb-4 text-xl font-semibold text-gray-700">Loading Filmkatalog...</h1>
+      <h1 className="mb-4 text-xl font-semibold text-gray-700">Laster Filmkatalog...</h1>
       <div className="w-16 h-16 border-t-4 border-blue-500 rounded-full animate-spin"></div>
     </div>
   );
