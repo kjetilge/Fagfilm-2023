6b00156d5debdf45e6abe44dc89b236bb58f9446
kjetilge <geirbo@icloud.com>
2023-11-07 02:24:11 +0100

    rep top nav

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -22,7 +22,7 @@ export default function NavBar({ session }: { session: Session | null }) {
         } z-30 transition-all`}
       >
         <div className="page-margins flex h-16 items-center justify-between w-full">
-          <div className="flex gap-10 items-center">
+          <div className="flex gap-2 md:gap-10 items-center">
             <Link href="/" className="flex items-center font-display text-2xl text-white">
               <Image
                 src="/logo.png"
@@ -31,15 +31,13 @@ export default function NavBar({ session }: { session: Session | null }) {
                 height="24"
                 className="mr-2 rounded-sm"
               ></Image>
-              <p className="subpixel-antialiased  ml-2 font-sans font-thin tracking-widest">Fagfilm</p>
+              <p className="subpixel-antialiased  ml-2 font-sans font-thin md:tracking-widest">Fagfilm</p>
             </Link>
-            <div className="mt-2 subpixel-antialiased text-base text-white font-thin flex gap-6">
+            <div className="mt-2 subpixel-antialiased text-sm md:text-base text-white font-thin flex gap-2 md:gap-6">
               <Link href="/filmkatalog" className="hover:text-gray-200">Filmkatalogen</Link>
               <Link href="/filmkatalog/yrkesforberedende">Filmkategori</Link>
             </div>
-
           </div>
-
           <div>
             {session ? (
               <UserDropdown session={session} />
